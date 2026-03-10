a1c476ef926f495719aa22b81ae87dc11d8d0bb3
rubaqeshta04 <qeshtaruba@gmail.com>
2026-03-10 11:42:43 +0200

    create home page

--- azkar/src/App.jsx
+++ azkar/src/App.jsx
@@ -1,13 +1,17 @@
 import Home from "./pages/Home";
+import AzkarDetail from "./components/AzkarDetail";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#347978]">
-      <div className="min-h-screen flex flex-col">
-        <Home />
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:type" element={<AzkarDetail />} />
+      </Routes>
+    </Router>
+
   );
 }
 
--- azkar/src/pages/Home.jsx
+++ azkar/src/pages/Home.jsx
@@ -1,16 +1,100 @@
 import React from "react";
-import azkar from "../data/azkar";
-import AzkarList from "../components/AzkarList";
+import { useNavigate } from "react-router-dom";
+
+// material ui
+import ImportContactsIcon from "@mui/icons-material/ImportContacts";
+import SunnyIcon from "@mui/icons-material/Sunny";
+import BedtimeIcon from "@mui/icons-material/Bedtime";
+import AlarmOnIcon from "@mui/icons-material/AlarmOn";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div className="min-h-screen  bg-[#004d40] flex items-center justify-center p-5">
-      <div className="bg-[#122d2d] rounded-lg p-6  shadow-lg  w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-4 text-center text-white mt-5">
-          أذكار اليوم
+    <div
+      className="min-h-screen bg-linear-to-br from-[#004d40] to-[#00796b] flex flex-col items-center justify-center p-6 dir-rtl"
+      dir="rtl"
+    >
+      <div className="w-full max-w-md text-center mb-10">
+        <div className="inline-block px-6 py-5 bg-white/10 backdrop-blur-md rounded-3xl mb-4 shadow-2xl">
+          <span className="text-8xl flex items-center text-white">
+            <ImportContactsIcon fontSize="large" />
+          </span>
+        </div>
+        <h1 className="text-4xl font-extrabold text-white mb-4 tracking-tight">
+          تطبيق الأذكار
         </h1>
-        <AzkarList azkar={azkar} />
+        <p className="text-teal-100 text-lg opacity-80">
+          «أَلَا بِذِكْرِ اللَّهِ تَطْمَئِنُّ الْقُلُوبُ»
+        </p>
       </div>
+
+      <div className="w-full max-w-md space-y-4">
+        <button
+          onClick={() => navigate("/morning")}
+          className="group relative w-full bg-white/10 backdrop-blur-lg border border-white/20 p-6 rounded-3xl flex items-center justify-between hover:bg-white/20 transition-all duration-300 shadow-xl overflow-hidden"
+        >
+          <div className="flex items-center gap-4">
+            <div className="w-12 h-12 text-[#004d40] bg-yellow-400/50 rounded-2xl flex items-center justify-center text-2xl group-hover:scale-110 transition-transform">
+              <SunnyIcon fontSize="medium" />
+            </div>
+            <div className="flex flex-col text-right ">
+              <h2 className="text-xl font-bold text-white">أذكار الصباح</h2>
+              <p className="text-sm text-teal-100/60  font-medium mt-2">
+                ابدأ يومك بذكر الله
+              </p>
+            </div>
+          </div>
+          <span className="text-white/40 group-hover:translate-x-2.5 transition-transform">
+            ←
+          </span>
+        </button>
+
+        <button
+          onClick={() => navigate("/evening")}
+          className="group relative w-full bg-white/10 backdrop-blur-lg border border-white/20 p-6 rounded-3xl flex items-center justify-between hover:bg-white/20 transition-all duration-300 shadow-xl overflow-hidden"
+        >
+          <div className="flex items-center gap-4">
+            <div className="w-12 h-12 text-[#004d40] bg-indigo-400/40 rounded-2xl flex items-center justify-center text-2xl group-hover:scale-110 transition-transform">
+              <BedtimeIcon fontSize="medium" />
+            </div>
+            <div className="flex flex-col text-right ">
+              <h2 className="text-xl font-bold text-white">أذكار المساء</h2>
+              <p className="text-sm text-teal-100/60 font-medium mt-2">
+                اختم يومك بذكر الله
+              </p>
+            </div>
+          </div>
+          <span className="text-white/40 group-hover:translate-x-2.5 transition-transform">
+            ←
+          </span>
+        </button>
+
+        <button
+          onClick={() => navigate("/sleep")}
+          className="group relative w-full bg-white/10 backdrop-blur-lg border border-white/20 p-6 rounded-3xl flex items-center justify-between hover:bg-white/20 transition-all duration-300 shadow-xl overflow-hidden"
+        >
+          <div className="flex items-center gap-4">
+            <div className="w-12 h-12 text-[#004d40] bg-indigo-400/60 rounded-2xl flex items-center justify-center text-2xl group-hover:scale-110 transition-transform">
+              <AlarmOnIcon fontSize="medium" />
+            </div>
+
+            <div className="flex flex-col text-right">
+              <h2 className="text-xl font-bold text-white">أذكار النوم</h2>
+              <p className="text-sm text-teal-100/60 font-medium mt-2">
+                اجعل آخر كلماتك قبل النوم ذكر الله
+              </p>
+            </div>
+          </div>
+          <span className="text-white/40 group-hover:translate-x-2.5 transition-transform">
+            ←
+          </span>
+        </button>
+      </div>
+
+      <footer className="mt-12 text-white/40 text-sm font-medium">
+        تم التطوير بكل ❤️ لخدمتكم
+      </footer>
     </div>
   );
 }
