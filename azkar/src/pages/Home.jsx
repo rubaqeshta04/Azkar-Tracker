18d3ab0a519801b2387921343bb8ef4b428f5d8f
rubaqeshta04 <qeshtaruba@gmail.com>
2026-03-11 12:06:43 +0200

    add button Comfortable eye position in home page

--- azkar/src/pages/Home.jsx
+++ azkar/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { HighContrastContext } from "../context/HighContrastContext";
 
 // material ui
 import ImportContactsIcon from "@mui/icons-material/ImportContacts";
@@ -9,28 +11,34 @@ import AlarmOnIcon from "@mui/icons-material/AlarmOn";
 
 export default function Home() {
   const navigate = useNavigate();
+  const { highContrast, toggleContrast } = useContext(HighContrastContext);
 
   return (
     <div
-      className="min-h-screen bg-gradient-to-br from-[#004d40] to-[#00796b] flex flex-col items-center justify-center p-6 dir-rtl"
-      dir="rtl"
+      className={`min-h-screen flex flex-col items-center justify-center p-6 dir-rtl 
+      ${highContrast ? "bg-[#1F3A5F]" : "bg-gradient-to-br from-[#004d40] to-[#00796b]"}`}
     >
-      {/* شعار وترويسة */}
-      <div className="w-full max-w-md text-center mb-10">
-        <div className="inline-block px-6 py-5 bg-white/10 backdrop-blur-md rounded-3xl mb-4 shadow-2xl">
-          <span className="text-8xl flex items-center justify-center text-white">
-            <ImportContactsIcon fontSize="large" />
-          </span>
+      <div className="absolute top-4 right-4">
+        <button
+          className="group relative w-full bg-white text-[#004d40] px-4 py-2 text-sm md:px-6 md:py-3 md:text-lg rounded-3xl flex items-center justify-between hover:bg-gray-100 hover:scale-[1.02] active:scale-95 transition-all duration-300 shadow-2xl border border-white/30"
+          onClick={toggleContrast}
+        >
+          {highContrast ? "إلغاء الوضع المريح" : "وضع مريح للعين"}
+        </button>
+      </div>
+      <div className="w-full max-w-md flex flex-col items-center mb-10">
+        <div className="w-20 h-20 md:w-30 md:h-30 flex items-center justify-center bg-white/10 backdrop-blur-md rounded-3xl shadow-2xl mb-4">
+          <ImportContactsIcon sx={{ fontSize: 50 }} className="text-white" />
         </div>
-        <h1 className="text-4xl font-extrabold text-white mb-4 tracking-tight">
+
+        <h1 className="text-3xl md:text-4xl font-extrabold text-white mb-2">
           تطبيق الأذكار
         </h1>
-        <p className="text-teal-100 text-lg opacity-80">
+        <p className="text-teal-100 text-sm md:text-lg opacity-80 text-center">
           «أَلَا بِذِكْرِ اللَّهِ تَطْمَئِنُّ الْقُلُوبُ»
         </p>
       </div>
 
-      {/* الأزرار */}
       <div className="w-full max-w-md space-y-4">
         {/* أذكار الصباح */}
         <button
@@ -38,7 +46,7 @@ export default function Home() {
           className="group relative w-full bg-white/10 backdrop-blur-lg border border-white/20 p-6 rounded-3xl flex items-center justify-between hover:bg-white/20 transition-all duration-300 shadow-xl overflow-hidden"
         >
           <div className="flex items-center gap-4">
-            <div className="w-14 h-14 p-2 text-[#004d40] bg-yellow-400/50 rounded-2xl flex items-center justify-center text-3xl group-hover:scale-110 transition-transform">
+            <div className="w-14 h-14 p-2 bg-yellow-400/40 rounded-2xl flex items-center justify-center text-3xl">
               <SunnyIcon fontSize="medium" />
             </div>
             <div className="flex flex-col text-right ">
@@ -59,7 +67,7 @@ export default function Home() {
           className="group relative w-full bg-white/10 backdrop-blur-lg border border-white/20 p-6 rounded-3xl flex items-center justify-between hover:bg-white/20 transition-all duration-300 shadow-xl overflow-hidden"
         >
           <div className="flex items-center gap-4">
-            <div className="w-14 h-14 p-2 text-[#004d40] bg-indigo-400/40 rounded-2xl flex items-center justify-center text-3xl group-hover:scale-110 transition-transform">
+            <div className="w-14 h-14 p-2 bg-indigo-400/40 rounded-2xl flex items-center justify-center text-3xl">
               <BedtimeIcon fontSize="medium" />
             </div>
             <div className="flex flex-col text-right ">
@@ -80,7 +88,7 @@ export default function Home() {
           className="group relative w-full bg-white/10 backdrop-blur-lg border border-white/20 p-6 rounded-3xl flex items-center justify-between hover:bg-white/20 transition-all duration-300 shadow-xl overflow-hidden"
         >
           <div className="flex items-center gap-6">
-            <div className="w-14 h-14 p-2 text-[#ffeb3b] bg-[#000000] rounded-2xl flex items-center justify-center text-3xl group-hover:scale-110 transition-transform">
+            <div className="w-14 h-14 p-2 bg-gray-800/60 rounded-2xl flex items-center justify-center text-3xl">
               <AlarmOnIcon fontSize="medium" />
             </div>
 
@@ -96,11 +104,6 @@ export default function Home() {
           </span>
         </button>
       </div>
-
-      {/* Footer */}
-      <footer className="mt-12 text-white/40 text-sm font-medium">
-        تم التطوير بكل ❤️ لخدمتكم
-      </footer>
     </div>
   );
 }
